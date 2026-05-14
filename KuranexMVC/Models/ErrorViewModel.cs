6b35bff0331e1e1049b5973ec743e0aecd8c7025
Gonzalo Cerqueiro <gmc199615@gmail.com>
2026-05-14 17:19:51 -0300

    Creacion de Kuranex MVC

---
+++ KuranexMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KuranexMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
